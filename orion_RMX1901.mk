d32a8b888516af7807a480757946f8a4eb8d4e80
ij-israfil <israfilkhan494@gmail.com>
2024-08-06 10:30:06 +0600

    RMX1901: Update sign keys path
    Signed-off-by: ij-israfil <israfilkhan494gmail.com>

--- orion_RMX1901.mk
+++ orion_RMX1901.mk
@@ -24,7 +24,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
 # Sign builds
-PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/.android-certs/releasekey
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey
 
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1901
